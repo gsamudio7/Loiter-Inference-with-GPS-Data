361049d7520a5bb1611baf73f825fb073f740b41
developer <gsamudio7@gmail.com>
2021-07-22 16:52:27 -0400

    so many fixes

--- scripts/inferLoiter.R
+++ scripts/inferLoiter.R
@@ -29,24 +29,47 @@ library(h3)
 use_python("/Users/developer/opt/anaconda3/bin/python", required=TRUE)
 
 
-source_python("scripts/pythonSupport.py")
+source_python("scripts/supportFunctions.py")
 
 # First chunk testing ####
-resetMap <- function(map) {
-  # Reference https://github.com/rstudio/leaflet/issues/623
-  map %>%
-    addEasyButton(
-      easyButton(
-        icon = "ion-arrow-shrink", 
-        title = "Reset View", 
-        onClick = JS(
-          "function(btn, map){ map.setView(map._initialCenter, map._initialZoom); }"
-        )
-      )
-    ) %>% 
-    htmlwidgets::onRender(
-      JS(
-        "
+baseLeaf <- function(
+  centerLat=55,
+  centerLon=-105,
+  initZoom=4,
+  logoLocation="https://github.com/gsamudio7/Loiter-Inference-with-GPS-Data/blob/main/assets/images/NGATitle.png?raw=true",
+  logoWidth=350,logoHeight=60,
+  gitRepo="https://github.com/gsamudio7/Loiter-Inference-with-GPS-Data",
+  gitRepoTitle="Loiter Inference with GPS Data",
+  groups2hide=NULL,
+  overGroups=NULL) {
+      return(
+        leaflet() %>% 
+          addProviderTiles(
+            group="NGA Imagery",
+            provider=providers$Esri.WorldImagery,
+            options=tileOptions(
+              attribution=toString(tags$a(href = paste0(gitRepo),
+                                          gitRepoTitle)))) %>%
+          addProviderTiles(
+            provider=providers$CartoDB.DarkMatter,
+            group="NGA Slate",
+            options=tileOptions(
+              attribution=toString(tags$a(href = paste0(gitRepo),
+                                          gitRepoTitle)))) %>%
+          addLogo(img=logoLocation,
+                  width=logoWidth,height=logoHeight,url=gitRepo) %>%
+          addEasyButton(
+            easyButton(
+              icon = "ion-arrow-shrink", 
+              title = "Reset View", 
+              onClick = JS(
+                "function(btn, map){ map.setView(map._initialCenter, map._initialZoom); }"
+              )
+            )
+          ) %>% 
+          htmlwidgets::onRender(
+            JS(
+              "
           function(el, x){ 
             var map = this; 
             map.whenReady(function(){
@@ -54,60 +77,66 @@ resetMap <- function(map) {
               map._initialZoom = map.getZoom();
             });
           }"
-      )
-    )
-}
-
-baseMap <- function(centerLat=55,
-                    centerLon=-105,
-                    initZoom=4,
-                    logoLocation="https://github.com/gsamudio7/Loiter-Inference-with-GPS-Data/blob/main/assets/images/NGATitle.png?raw=true",
-                    logoWidth=350,logoHeight=60,
-                    gitRepo="https://github.com/gsamudio7/Loiter-Inference-with-GPS-Data",
-                    gitRepoTitle="Loiter Inference with GPS Data",
-                    groups2hide=NULL,
-                    overGroups=NULL) {
-  return(
-    leaflet() %>% 
-      addProviderTiles(
-        group="NGA Imagery",
-        provider=providers$Esri.WorldImagery,
-        options=tileOptions(
-          attribution=toString(tags$a(href = paste0(gitRepo),
-                                      gitRepoTitle)))) %>%
-      addProviderTiles(
-        provider=providers$CartoDB.DarkMatter,
-        group="NGA Slate",
-        options=tileOptions(
-          attribution=toString(tags$a(href = paste0(gitRepo),
-                                      gitRepoTitle)))) %>%
-      addLogo(img=logoLocation,
-              width=logoWidth,height=logoHeight,url=gitRepo) %>%
-      resetMap() %>%
-      addMouseCoordinates() %>%
-      setView(lng=centerLon,lat=centerLat,zoom=initZoom) %>%
-      addMiniMap(
-        tiles='Esri.WorldImagery',
-        zoomLevelOffset = -10,
-        toggleDisplay = TRUE, 
-        position="bottomleft") %>%
-      addFullscreenControl() %>%
-      addLayersControl(
-        baseGroups = c("NGA Slate","NGA Imagery"),
-        overlayGroups = overGroups,
-        options = layersControlOptions(collapsed = FALSE)) %>%
-      hideGroup(c("NGA Imagery",groups2hide)) %>%
-      
-      addControl(
-        className=NULL,
-        html=
-          "<style>
-          .leaflet-control-layers-expanded {
-              padding: 6px 10px 6px 6px;
-              color: #fff;
-              background: #333;
-          </style>")
-  )}
+            )
+          ) %>%
+          addMouseCoordinates() %>%
+          setView(lng=centerLon,lat=centerLat,zoom=initZoom) %>%
+          addMiniMap(
+            tiles='Esri.WorldImagery',
+            zoomLevelOffset = -10,
+            toggleDisplay = TRUE, 
+            position="bottomleft") %>%
+          addLayersControl(
+            baseGroups = c("NGA Slate","NGA Imagery"),
+            overlayGroups = overGroups,
+            options = layersControlOptions(collapsed = FALSE)) %>%
+          hideGroup(c("NGA Imagery",groups2hide)) %>%
+          
+          addControl(
+            className=NULL,
+            html=
+              "<style>
+              .leaflet-control-layers-expanded {
+                  padding: 6px 10px 6px 6px;
+                  color: #fff;
+                  background: #333;
+              }
+              
+              .info {
+                padding: 6px 8px;
+                font: 14px/16px Arial, Helvetica, sans-serif;
+                background: #333;
+              }
+              
+              .legend svg text {
+                fill: #fff;
+              }
+              
+              .legend {
+                color: #fff;
+              }
+              
+              .legend svg line {
+                stroke: #fff;
+              }
+              
+              .leaflet-bar a, .leaflet-bar a:hover {
+                background-color: #333;
+                border-bottom: 1px solid #ccc;
+                text-align: center;
+                text-decoration: none;
+                color: #fff;
+              }
+              
+              .leaflet-bar button, .leaflet-bar button:hover {
+                background-color: #333;
+                text-align: center;
+                text-decoration: none;
+                color: #fff;
+              }
+              
+              </style>")
+      )}
 
 
 
@@ -118,21 +147,18 @@ hexFrame_2_sf = function(hexFrame) {
   return(st_as_sf(hexFrame, bdry$geometry))
 }
 
-plotH3 <- function(map,pts,h3Resolution,groupName,colPal) {
+plotH3 <- function(map,pts,h3Resolution,groupName,colPal,h3_labels=NULL) {
   
   # Set H3 column
   pts[,"H3" := geo_to_h3(pts[,c("lat","lon")],res=h3Resolution)]
   
-  # Set count data.table
-  cdt <- pts[,.(count=.N),by=H3]
-  m <- sum(cdt[,count])
+  # Set count data.table and make sf object
+  hexFrame <- pts[,.(count=.N),by=H3] %>% hexFrame_2_sf()
+  m <- sum(hexFrame$count)
   
   # Set color palette
   pal <- colorNumeric(palette=colPal,reverse=TRUE,
-                      domain=log10(cdt[,count]))
-  
-  # Make sf object
-  hexFrame <- hexFrame_2_sf(cdt)
+                      domain=log10(hexFrame$count))
   
   # Map it
   return(map %>% 
@@ -145,13 +171,13 @@ plotH3 <- function(map,pts,h3Resolution,groupName,colPal) {
                 color="white",
                 fillOpacity = 0.7,
                 bringToFront = TRUE),
-              #label=labels,
+              label=h3_labels,
               group = groupName
           ) %>% 
            addLegend(position="bottomright",
                      group=groupName,
-                     pal=pal,opacity=1,
-                     values=log10(cdt[,count]),
+                     pal=pal,opacity=8,
+                     values=log10(hexFrame$count),
                      labFormat=labelFormat(transform = function(x) {10^x %>% round()}),
                      title="<b>Obs Count</b>") %>%
            
@@ -184,7 +210,7 @@ wolf.of.Interest <- data[,.(count=.N),by="cid"][count==max(count)]$cid
 dt <- data[cid==wolf.of.Interest]
 
 # Plot H3 density map of wolf population and wolf of interest
-baseMap(overGroups=c("Wolf Population","Wolf of Interest")) %>% 
+baseLeaf(overGroups=c("Wolf Population","Wolf of Interest")) %>% 
   
   plotH3(pts=data[,c("lon","lat")],
          h3Resolution=8,
@@ -198,15 +224,7 @@ baseMap(overGroups=c("Wolf Population","Wolf of Interest")) %>%
   
 
 
-  plotGeohash(pts = data[,c("lon","lat")],
-              geohashPrecision = 6,
-              groupName = "All Wolves",
-              colPal="Reds") %>%
-  
-  plotGeohash(pts = dt[,c("lon","lat")],
-              geohashPrecision = 6,
-              groupName = "Wolf of Interest",
-              colPal="Blues")
+
 
 
 
